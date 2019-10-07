f4ebd0df61f04443eb4b53e5268cdb510fe0f962
davidlawrencer <davidlrifkin+github@gmail.com>
2019-10-07 09:48:56 -0400

    Adds green mode

--- intro-to-programmatic-ui/ViewController.swift
+++ intro-to-programmatic-ui/ViewController.swift
@@ -10,11 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private var greenModeStatus = false {
+        didSet {
+            refreshGreenMode()
+        }
+    }
+    
+    @IBOutlet weak var statusLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        greenModeStatus = true
     }
 
-
+    @IBAction func modeSwitchPressed(_ sender: UISwitch) {
+        self.greenModeStatus = sender.isOn        
+    }
+    
+    private func refreshGreenMode() {
+        if greenModeStatus {
+            self.view.backgroundColor = .green
+            self.statusLabel.textColor = .white
+            self.statusLabel.text = "Green Mode is on!"
+        } else {
+            self.view.backgroundColor = .white
+            self.statusLabel.textColor = .green
+            self.statusLabel.text = "Green Mode is off!"
+        }
+    }
 }
 
